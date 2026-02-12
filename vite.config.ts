40b62615527f67f6a1bb657d408befac1903111d
Moha2001-dev <80723154+Moha2001-dev@users.noreply.github.com>
2026-02-12 16:57:45 +0300

    Update vite.config.ts for GitHub Pages configuration and build settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,13 @@
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/count-down/',
-  plugins: [vue()],
   build: {
-    outDir: 'dist', // specify output directory
-    emptyOutDir: true, // clean the output directory before building
-    rollupOptions: {
-      // Additional Rollup options here if needed
+    outDir: 'dist',
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
     },
   },
 });
